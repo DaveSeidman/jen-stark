5b50ad2ee816853357a1ce9d74c335b8f110da69
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-15 12:10:51 -0400

    first model

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { OrbitControls, PerspectiveCamera, Stars, useGLTF } from '@react-three/drei';
+import sceneFile from './assets/models/scene1.glb';
 import './index.scss';
 
-function App() {
+function Model() {
+  const gltf = useGLTF(sceneFile);
+  return <primitive object={gltf.scene} />;
+}
+
+function Scene() {
+  const cameraRef = useRef();
 
   return (
-    <div className="app">
-      <h1>Jen Stark</h1>
-      <p>Interactive Gallery</p>
-    </div>
+    <Canvas
+      camera={(
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 0, 5]}
+          ref={cameraRef}
+        />
+      )}
+    >
+      <ambientLight intensity={0.5} />
+      <spotLight intensity={0.8} position={[300, 300, 400]} />
+
+      <Model />
+
+      <OrbitControls enablePan enableZoom enableRotate enableDamping dampingFactor={0.2} target={[0, 0, 0]} />
+      <Stars />
+    </Canvas>
   );
 }
 
-export default App;
+export default Scene;
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  assetsInclude: ['**/*.glb'],
   server: {
     port: 8080,
     host: true,
