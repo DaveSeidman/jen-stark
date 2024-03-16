df42f67db03730ebd41faa3e39ddb84bae3c497f
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-16 09:44:19 -0400

    view swapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Vector3, CameraHelper, CatmullRomCurve3 } from 'three';
+import { Vector3, CameraHelper, CatmullRomCurve3, MeshBasicMaterial } from 'three';
 import { useHelper, PerspectiveCamera, Stars, useGLTF, Environment } from '@react-three/drei';
 import sceneFile from './assets/models/scene1.glb';
 import envFile from './assets/images/metro_noord_4k.hdr';
@@ -10,8 +10,15 @@ import { points } from './assets/models/path1.json';
 
 function Model() {
   const { scene, animations } = useGLTF(sceneFile);
-
+  const wireMat = new MeshBasicMaterial({ wireframe: true });
   console.log(scene.getObjectByName('Art1'));
+
+  scene.traverse((obj) => {
+    console.log(obj.isMesh);
+    if (obj.isMesh) {
+      // obj.material = wireMat;
+    }
+  });
 
   const mixerRef = useRef();
 
@@ -24,7 +31,7 @@ function Model() {
 }
 
 const curve = new CatmullRomCurve3(points.map((p) => new Vector3(p.x, p.y, p.z)));
-curve.closed = true;
+// curve.closed = true;
 const lookAt = new Vector3();
 const lookAhead = 0.05;
 const tourSpeed = -0.0005;
@@ -35,13 +42,12 @@ function TourCamera({ makeDefault }) {
   const progressTarget = useRef(0);
 
   const cameraRef = useRef();
-  // useHelper(cameraRef, CameraHelper, 'cyan');
+  const helper = useHelper(cameraRef, CameraHelper, 'cyan');
 
   useFrame(() => {
     setProgress((prevProgress) => (progressTarget.current - prevProgress) / 10);
 
     const { x, y, z } = curve.getPoint(progress);
-    // a point just ahead of progress on the curve
     curve.getPoint(progress + lookAhead, lookAt);
     setCameraPosition([x, y, z]);
     cameraRef.current.lookAt(lookAt);
@@ -69,6 +75,8 @@ function TourCamera({ makeDefault }) {
       ref={cameraRef}
       position={cameraPosition}
       makeDefault={makeDefault}
+      fov={50}
+      far={20}
     />
   );
 }
@@ -77,8 +85,9 @@ function OverviewCamera({ makeDefault }) {
   return (
     <PerspectiveCamera
       makeDefault={makeDefault}
-      position={[0, 4, -4]}
-      rotation={[0.1, 0, 0]}
+      position={[0, 80, 100]}
+      rotation={[-38 * (Math.PI / 180), 0, 0]}
+      fov={20}
     />
   );
 }
@@ -87,13 +96,12 @@ function Scene({ overview }) {
   return (
     <Canvas
       linear
-      gl={{ toneMapping: 1, toneMappingExposure: 0.1 }}
+      gl={{ toneMapping: 2, toneMappingExposure: 1 }}
     >
+      <Environment files={envFile} background />
       <TourCamera makeDefault={!overview} />
       <OverviewCamera makeDefault={overview} />
       <Model />
-      <Environment files={envFile} background />
-      <Stars />
     </Canvas>
   );
 }
