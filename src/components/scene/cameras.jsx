11b4e42c3d4a206f0cadbf2b31d1e563ba9d1079
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-23 08:22:02 -0400

    tilt controls

--- src/components/scene/cameras.jsx
+++ src/components/scene/cameras.jsx
@@ -1,7 +1,9 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { Vector3, CameraHelper, AxesHelper, CatmullRomCurve3 } from 'three';
-import { useHelper, PerspectiveCamera } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import { Vector2, Vector3, CameraHelper, AxesHelper, CatmullRomCurve3 } from 'three';
+import { useHelper, PerspectiveCamera, OrbitControls } from '@react-three/drei';
 import { points } from '../../assets/models/camera-path.json';
 
 const curve = new CatmullRomCurve3(points.map((p) => new Vector3(p.x, p.y, p.z)));
@@ -10,21 +12,36 @@ const lookAt = new Vector3();
 export function TourCamera({ makeDefault, scrollPercent, lookAhead }) {
   const [containerPosition, setContainerPosition] = useState([0, 0, 0]);
   const progress = useRef(0);
-
+  const { gl } = useThree();
   const containerRef = useRef();
   const cameraRef = useRef();
+  const rotationTarget = useRef(new Vector2())
+
   useHelper(cameraRef, makeDefault ? AxesHelper : CameraHelper, 'cyan');
   useFrame(() => {
-    progress.current += (scrollPercent - progress.current) / 20;
-    try {
-      const { x, y, z } = curve.getPoint(progress.current);
-      curve.getPoint(progress.current + lookAhead, lookAt);
-      setContainerPosition([x, y, z]);
-      containerRef.current.lookAt(lookAt);
-    } catch (err) {
-      console.log('err', progress.current);
-    }
+    progress.current += (scrollPercent - progress.current) / 20; // TODO: make 20 a variable and base on frame delta
+    const { x, y, z } = curve.getPoint(progress.current);
+    curve.getPoint(progress.current + lookAhead, lookAt);
+    setContainerPosition([x, y, z]);
+    containerRef.current.lookAt(lookAt);
+    cameraRef.current.rotation.x += (rotationTarget.current.x - cameraRef.current.rotation.x) / 20;
+    cameraRef.current.rotation.y += (rotationTarget.current.y - cameraRef.current.rotation.y) / 20;
   });
+
+  const pointerMove = (e) => {
+    const { clientX, clientY } = e;
+    const { width, height } = gl.domElement.getBoundingClientRect()
+    rotationTarget.current.y = Math.PI - ((clientX / width) - .5) * .5;
+    rotationTarget.current.x = ((clientY / height) - .5) * .5;
+  };
+
+  useEffect(() => {
+    addEventListener('pointermove', pointerMove);
+
+    return () => {
+      removeEventListener('pointermove', pointerMove);
+    };
+  }, []);
 
   return (
     <group
@@ -33,7 +50,7 @@ export function TourCamera({ makeDefault, scrollPercent, lookAhead }) {
     >
       <PerspectiveCamera
         ref={cameraRef}
-        rotation={[0, Math.PI, 0]}
+        rotation={[0, 0, 0]}
         makeDefault={makeDefault}
         fov={makeDefault ? 60 : 30}
         far={makeDefault ? 1000 : 10}
