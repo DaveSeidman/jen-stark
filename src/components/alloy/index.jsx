77dd21639ea29c2dbcacecd28f13a0d984aee692
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-28 07:02:10 -0400

    alloy gallery working

--- src/components/alloy/index.jsx
+++ src/components/alloy/index.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './index.scss';
-
+import logo from '../../assets/images/AlloyLogo.svg';
 import { carousels } from '../../../config.json'
-console.log(carousels.alloy);
 
 function Alloy() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => prevIndex < carousels.alloy.length - 1 ? prevIndex + 1 : 0)
+    }, 6000);
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
 
   return (
     <div className="alloy">
-      <div className="allow-carousel">
+      <div className="alloy-carousel">
         {
-          carousels.alloy.map((image) => (
+          carousels.alloy.map((image, index) => (
             <div
               key={image}
-              className="allow-carousel-image"
-              style={{ background: `alloy/${image}` }}></div>
+              className={`alloy-carousel-image ${index === currentIndex ? '' : 'hidden'}`}
+              style={{ backgroundImage: `url('${location.pathname}/alloy/${image}'` }}></div>
           ))
         }
       </div>
 
-      <div className="allow-logo"></div>
+      <img className="alloy-logo" src={logo} />
     </div >
   )
 }
