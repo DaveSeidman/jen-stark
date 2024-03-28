d52d3542c059ce5dcc71e9dd004985ee8a8ee028
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-28 07:56:28 -0400

    opportunities section started

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React, { useRef, useState, useEffect } from 'react';
 import mapFile from './assets/images/map2.png';
 import './index.scss';
 import { pages } from '../config.json';
-// import Scene from './components/scene';
 import Scene from './components/scene';
 import Carousel from './components/carousel';
 import Nav from './components/nav';
 import Experience from './components/experience';
 import Artist from './components/artist';
 import Venue from './components/venue';
-// import Partner from './components/partner';
-// import Footer from './components/footer';
+import Opportunities from './components/opportunities';
 import VideoCover from './components/videocover';
 import VideoBreak from './components/videobreak';
 import Alloy from './components/alloy';
@@ -52,8 +50,7 @@ function App() {
       <Alloy />
       <Venue />
 
-      {/* <Partner /> */}
-      {/* <Footer /> */}
+      <Opportunities />
       <VideoCover></VideoCover>
       <p className="version">{`version: ${version}`}</p>
     </div>
---
+++ src/components/opportunities/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.scss';
+
+function Opportunities() {
+  return (
+    <div id="opportunities" className="opportunities">
+      <div className="opportunities-body">
+        <h1 className="opportunities-title">Opportunities</h1>
+        <p>Anticipated to attract 50,000 visitors over five months, Cascade offers vast sponsorship opportunities and audience engagement, bolstered by a promotional campaign to a 400,000-strong social media following. It will enable meaningful brand interactions, client acquisition, and innovative food, beverage, and merchandising partnerships.</p>
+        <div className="opportunities-body-levels">
+          <div className='opportunities-body-levels-level'>
+            <h2>Partnerships</h2>
+            <p>Let's work together to shape a custom strategic investment opporunity for meaningful returns.</p>
+            <button type="button">Email Us</button>
+          </div>
+          <div className='opportunities-body-levels-level'>
+            <h2>Sponsors</h2>
+            <p>From the Cascade Lounge to our Bar, Digital to OOH, we have numbers ways for your brand to get involved.</p>
+            <button type="button">Email Us</button>
+          </div>
+          <div className='opportunities-body-levels-level'>
+            <h2>Patronage</h2>
+            <p>Looking to be an essential part of making Cascade a reality? Learn more about becoming a Patron.</p>
+            <button type="button">Email Us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Opportunities;
