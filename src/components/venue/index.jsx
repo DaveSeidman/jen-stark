f65b5a83e103fa7d5eb620d08e8672370c227200
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2024-03-29 11:40:41 -0400

    socials

--- src/components/venue/index.jsx
+++ src/components/venue/index.jsx
@@ -7,7 +7,7 @@ function Venue() {
   return (
     <div id="venue" className="venue">
       <div className='venue-body'>
-        <div className="venue-body-map" />
+        <div className="venue-body-map" onClick={() => { window.open('https://maps.app.goo.gl/SCEKdFRQ3K8WGdMs6') }} />
         <div className="venue-body-text back-purple">
           <h2>Venue</h2>
           <p>In the heart of LA's Arts District, a new center of energy has emerged: a multifaceted community where work, retail, and living collide. Welcome to <a href="https://alloyla.com" target="_blank">Alloy</a>, a vibrant place for creatives, artists, and seekers alike. Here, each day brings new experiences and fresh encounters with a lively pedestrian paseo that connects it all.</p>
